04f5fb6ee6ce19a1ef13a6aceddfca3575896301
Mili Akther <miliakthermilu@gmail.com>
2025-05-12 23:08:52 +0600

    update user login info in the database

---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,87 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../Provider/AuthProvider';
+
+const SignUp = () => {
+      const { createUser } = useContext(AuthContext);
+
+      const handleSignUp = e =>{
+            e.preventDefault();
+
+            const name = e.target.name.value;
+            const email = e.target.email.value;
+            const password = e.target.password.value;
+            console.log('form sign up',email,password);
+
+
+            createUser(email,password)
+            .then(result=>{
+                  console.log('user created at firebase',result.user);
+                  const createdAt = result?.user?.metadata?.creationTime;
+                  const newUser = { name, email,createdAt };
+                  // save new user info to the database
+                  fetch("http://localhost:5000/users ", {
+                    method: "POST",
+                    headers: {
+                      "content-type": "application/json",
+                    },
+                    body: JSON.stringify(newUser),
+                  })
+                    .then((res) => res.json())
+                    .then((data) => {
+                      
+                      if(data.insertedId){
+                        console.log('user created in db');
+                      }
+                    });
+
+            })
+            .catch(error => {
+                  console.log('error',error);
+            })
+
+      }
+
+      return (
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+              <h1 className="text-5xl font-bold">Sign Up now!</h1>
+              <p className="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+            </div>
+            <form onSubmit={handleSignUp}>
+                  
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+              <div className="card-body">
+                <fieldset className="fieldset">
+                  <label className="label">Name</label>
+                  <input type="text" className="input" placeholder="name" name="name" />
+
+                  <label className="label">Email</label>
+                  <input type="email" className="input" placeholder="Email" name="email" />
+
+                  <label className="label">Password</label>
+                  <input
+                    type="password"
+                    className="input"
+                    placeholder="Password" name="password"
+                  />
+                  <div>
+                    <a className="link link-hover">Forgot password?</a>
+                  </div>
+                  <button className="btn btn-neutral mt-4">
+                        Sign Up
+                  </button>
+                </fieldset>
+              </div>
+            </div>
+            </form>
+          </div>
+        </div>
+      );
+};
+
+export default SignUp;
